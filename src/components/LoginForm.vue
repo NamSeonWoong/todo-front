1b11d2d3c39ca11b61d6e04ddce644389adc14c0
남선웅 <wun1107@gmail.com>
2019-11-19 11:08:55 +0900

    login 완료

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,22 +1,76 @@
 <template>
   
-  <div class="login-div col-6 offset-3">
-    <div class="form-group">
-      <label for="id">ID</label>
-      <input id="id" class="form-control" type="text">
+  <div>
+
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
     </div>
 
-    <div class="form-group">
-      <label for="password">PASSWORD</label>
-      <input id="password" class="form-control" type="password">
+    <div v-else class="login-div col-6 offset-3">
+      <div v-if="errors.length" class="error-list alert alert-danger">
+        <div v-for="(error, idx) in errors" :key="idx">
+          {{error}}
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input id="id" class="form-control" type="text" v-model="credential.username">
+      </div>
+
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <input id="password" class="form-control" type="password" v-model="credential.password">
+      </div>
+      <button class="btn btn-primary" @click="login">로그인</button>
     </div>
-    <button class="btn btn-primary">로그인</button>
   </div>
+  
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import router from '../router'
 
+export default {
+ data:function(){
+  return{
+    credential: {
+      username: '',
+      password: '',
+    },
+    loading:false,
+    errors: [],
+  }
+ },
+ methods: {
+   login: function(){
+     if(this.checkForm()){
+       console.log('로그인 시도!!')
+     axios.post('http://localhost:8000/api-token-auth/', this.credential)
+     .then((res)=>{
+       this.loading=true
+       res.data.token
+       this.$session.start()
+       this.$session.set('jwt',res.data.token)
+
+       router.push('/')
+     })
+     .catch((e)=>{
+       this.loading=true
+       console.log(e)
+     })
+     }
+   },
+   checkForm(){
+     this.error=[]
+     if (this.credential.password.length < 8) {this.errors.push("비밀번호는 8글자가 넘어야합니다.")}
+     if (!this.credential.username) {this.errors.push("아이디를 입력해주시요")}
+     console.log(this.errors)
+     if (this.errors.length === 0) {
+       return true
+     }
+   } 
+ }
 }
 </script>
 
